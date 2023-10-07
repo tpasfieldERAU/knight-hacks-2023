ceed4be0decc033f914363d488407e4cb114274c
victoneux <76826656+victoneux@users.noreply.github.com>
2023-10-06 23:31:26 -0400

    Add secrets file, do not run if secrets DNE

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import flask
+import json
+import sys
 
 app = flask.Flask(__name__)
+
+try:
+    f = open("secrets.json")
+    secrets = json.load(f)
+    f.close()
+except:
+    sys.exit()
+    print("Secrets file does not exist! ADD IT :(")
+    # It's a JSON file.
+    # dict with username and password values for opensky network login
 
 
 @app.route("/")
