0d415102dc2a00300bfadf33a3bd58534e3c4b84
tpasfieldERAU <111624637+tpasfieldERAU@users.noreply.github.com>
2023-10-07 22:41:02 -0400

    Formatting and organization maintenance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-## -------------- IMPORTS ---------------
+# -------------- IMPORTS --------------
 # External Imports
 import flask
 from flask import jsonify, request
@@ -10,23 +10,22 @@ import airportsdata as apd
 # Python Standard Library Imports
 import json
 import sys
-import threading
 import time
-## ------------ END IMPORTS -------------
 
-## --------- SECRETS MANAGEMENT ---------
+
+# --------- SECRETS MANAGEMENT ---------
 try:
     f = open("secrets.json")
     secrets = json.load(f)
     f.close()
-except:
+except FileNotFoundError:
     print("Secrets file does not exist! ADD IT :(")
     sys.exit()
     # It's a JSON file.
     # dict with username and password values for opensky network login
-## ------- END SECRETS MANAGEMENT -------
 
-## ---------- VARIABLE INITS ------------
+
+# ---------- VARIABLE INITS ------------
 # IDK, you can comment this lmao
 state_history = [[{
     "icao24": "xyz123",
@@ -49,30 +48,19 @@ airport = airports['KDAB']
 lat = airport['lat']
 lon = airport['lon']
 
-# Coordinates for initial load.
-#   Kinda just here for demo purposes. Will be removed later.
-initial_coordinates = [
-    {"lat": 51.505, "lon": -0.09, "name": "Plane 1"},
-    {"lat": 40.7128, "lon": -74.006, "name": "Plane 2"}
-]
-## ---------- END VARIABLE INITS ----------
 
-## --------- START AJAX FUNCTIONS ---------
+# --------- START AJAX FUNCTIONS ---------
 # Index page load
 @web_app.route("/")
 def hello_world():
     return flask.render_template('index.html')
+
 
 # Aircraft State Vector Management
 @web_app.route("/states")
 def states():
     return state_history[len(state_history)-1]
 
-# Get initial marker coordinates
-# Gets initial coordinates for demo. Overall unnecessary.
-@web_app.route('/get_coordinates', methods=['GET'])
-def get_coordinates():
-    return jsonify(initial_coordinates)
 
 # Update coordinates in some form. This can later call the API.
 @web_app.route('/update_coordinates', methods=['POST'])
@@ -82,24 +70,26 @@ def update_coordinates():
     ]
     return jsonify(new_coords)
 
+
 # Set airport based on form input.
 @web_app.route('/set_airport', methods=['POST'])
 def set_airport():
+    global airport
     data = request.get_json()
     icao = str(data)
 
     try:
         airport = airports[icao]
-    except:
+    except KeyError:
         # Reset to default
         airport = airports['KDAB']
         print("Error in airport search. Likely invalid ICAO code.")
 
     airport_coords = {'lat': airport['lat'], 'lon': airport['lon']}
     return jsonify(airport_coords)
-## -------- END AJAX FUNCTIONS --------
 
-## -------- GENERAL FUNCTIONS ---------
+
+# -------- GENERAL FUNCTIONS ---------
 def get_updated_states():
     sys.stdout.flush()
     try:
@@ -107,6 +97,8 @@ def get_updated_states():
         print("gotten states owo", flush=True)
         state_history.append(new_states)
     except:
+        # This one except case is the only formatting warning I
+        # can't remove since I don't know what it will throw.
         print("unable to get states", flush=True)
     time.sleep(30)
 
